32314c86b53fad9629edab3a7b87ab4ffa177e1e
Adeemma02 <emm.bolu@gmail.com>
2024-12-27 00:43:52 +0100

    web

--- src/Component/Project/Project.js
+++ src/Component/Project/Project.js
@@ -30,7 +30,7 @@ const Project = () => {
            <div className='pro'>
             <div className='project-title'>Nacos Website</div>
             <a
-        href="https://nacos-black.vercel.app/"  
+        href="https://nacos-orcin.vercel.app/"  
         target="_blank"
         rel="noopener noreferrer"
         style={{ textDecoration: "none" }}
