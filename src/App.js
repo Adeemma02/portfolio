2d7ebfaacb70384e5deae4d6da42e1950ce26f37
Adeemma02 <emm.bolu@gmail.com>
2025-04-06 15:17:48 +0100

    u

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Routes>
+      <Routes> 
         <Route path="/" element={<Home />} />
         <Route path="/Navbar" element={<Navbar />} />
         <Route path="/Project" element={<Project />} />
